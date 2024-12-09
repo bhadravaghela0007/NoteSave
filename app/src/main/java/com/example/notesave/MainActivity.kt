a62bfe7ed2de634e6bc90df39fd49f3119878d3e
bhadravaghela0007 <bhadravaghela25@gmail.com>
2024-12-09 11:46:08 +0530

    new update

--- app/src/main/java/com/example/notesave/MainActivity.kt
+++ app/src/main/java/com/example/notesave/MainActivity.kt
@@ -3,47 +3,56 @@ package com.example.notesave
 import android.content.Intent
 import android.database.Cursor
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.BorderStroke
-import androidx.compose.foundation.background
-import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.MoreVert
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SearchBar
+import androidx.compose.material3.SearchBarDefaults
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TopAppBar
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.Shadow
 import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
-import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.notesave.ui.theme.NoteSaveTheme
 
 class MainActivity : ComponentActivity() {
-    companion object
-    { lateinit var data: MydataClass }
+    companion object {
+        lateinit var data: MydataClass
+    }
 
+    @OptIn(ExperimentalMaterial3Api::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
 
+        var query = mutableStateOf("")
+        var activeStatus = mutableStateOf(false)
         data = MydataClass(this)
         setContent {
             NoteSaveTheme {
@@ -52,22 +61,53 @@ class MainActivity : ComponentActivity() {
 
                 var allDatalist = fetchAllData()
 
-                Scaffold(
-                    modifier = Modifier.fillMaxSize(),
-                    floatingActionButton = {
-                        FloatingActionButton(
-                            onClick = {
-                                val intent = Intent(this@MainActivity, InnerScreen::class.java)
-                                startActivity(intent)
-                            }, containerColor = MaterialTheme.colorScheme.primary,
-                            contentColor = Color.White
-                        ) {Icon(imageVector = Icons.Filled.Add, contentDescription = "Add Contact") }
-                    }
-                ) { innerPadding ->
-                    Column(modifier = Modifier.padding(top = 20.dp))
-                    {
+                Scaffold(topBar = {
+                    SearchBar(
+                        query = query.value,
+                        onQueryChange = {
+                            query.value = it
+                            Log.d("============", "onchange: $it")
+                        },
+                        onSearch = {
+                            Log.d("============", "search: $it")
+                        },
+                        active = activeStatus.value,
+                        onActiveChange = {
+                            activeStatus.value = it
+                        },
+                        placeholder = { Text("Search Here..") },
+                        leadingIcon = { Icon(Icons.Default.Search, contentDescription = null)},
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(15.dp)
+                    ) {
+
 
                     }
+                }, modifier = Modifier.fillMaxSize(), floatingActionButton = {
+                    FloatingActionButton(
+                        onClick = {
+                            val intent = Intent(this@MainActivity, InnerScreen::class.java)
+                            startActivity(intent)
+                        },
+                        containerColor = MaterialTheme.colorScheme.primary,
+                        contentColor = Color.White
+                    ) {
+                        Icon(
+                            imageVector = Icons.Filled.Add, contentDescription = "Add Contact"
+                        )
+                    }
+                }) { innerPadding ->
+
+                    Log.d("==========", "onCreate: $innerPadding")
+                    val lightColors = listOf(
+                        Color(0xFFFBEFEF), // light red
+                        Color(0xFFEFF7FB), // light blue
+                        Color(0xFFEFFBF3), // light green
+                        Color(0xFFFFF8E1), // light yellow
+                        Color(0xFFF7EFFB), // light purple
+                        Color(0xFFEFFBFA) // light cyan
+                    )
 
                     LazyVerticalGrid(
                         modifier = Modifier
@@ -76,25 +116,27 @@ class MainActivity : ComponentActivity() {
                         columns = GridCells.Fixed(2)
                     ) {
                         items(allDatalist.size) { index ->
-                            Surface(shadowElevation = 10.dp ,
+                            val backgroundcolor = lightColors[index % lightColors.size]
+
+                            Surface(
+                                shadowElevation = 10.dp,
+                                color = backgroundcolor,
                                 modifier = Modifier
                                     .padding(
                                         start = 15.dp, top = 15.dp, end = 15.dp, bottom = 15.dp
                                     )
                                     .height(200.dp)
-                                    .clickable
-                                    {
-                                        val intent = Intent(this@MainActivity, InnerScreen::class.java)
-//                                        intent.putExtra("allData","${allDatalist[index].title},${allDatalist[index].subtitle}")
-                                        intent.putExtra("allData",allDatalist[index])
+                                    .clickable {
+                                        val intent =
+                                            Intent(this@MainActivity, InnerScreen::class.java)
+                                        intent.putExtra("allData", allDatalist[index])
                                         startActivity(intent)
-                                    }, shape = RoundedCornerShape(10.dp)
+                                    },
+                                shape = RoundedCornerShape(10.dp)
                             ) {
                                 Column(
-                                    modifier = Modifier
-                                        .padding(top = 10.dp, start = 15.dp)
-                                )
-                                {
+                                    modifier = Modifier.padding(top = 10.dp, start = 15.dp)
+                                ) {
                                     Text(
                                         text = "Date",
                                         fontSize = 16.sp,
@@ -103,10 +145,8 @@ class MainActivity : ComponentActivity() {
                                     )
                                 }
                                 Column(
-                                    modifier = Modifier
-                                        .padding(top = 35.dp, start = 15.dp)
-                                )
-                                {
+                                    modifier = Modifier.padding(top = 35.dp, start = 15.dp)
+                                ) {
                                     Text(
                                         text = allDatalist[index].title,
                                         fontSize = 20.sp,
@@ -114,10 +154,8 @@ class MainActivity : ComponentActivity() {
                                     )
                                 }
                                 Column(
-                                    modifier = Modifier
-                                        .padding(top = 58.dp, start = 15.dp)
-                                )
-                                {
+                                    modifier = Modifier.padding(top = 58.dp, start = 15.dp)
+                                ) {
                                     Text(
                                         text = allDatalist[index].subtitle,
                                         fontSize = 20.sp,
@@ -132,11 +170,10 @@ class MainActivity : ComponentActivity() {
         }
     }
 
-    private fun fetchAllData(): ArrayList<DataDetailsModel>
-    {
+    private fun fetchAllData(): ArrayList<DataDetailsModel> {
         val dataList = ArrayList<DataDetailsModel>()
 
-        val cursor : Cursor = data.readableDatabase.rawQuery("SELECT * FROM note",null)
+        val cursor: Cursor = data.readableDatabase.rawQuery("SELECT * FROM note", null)
 
         cursor.use {
 
